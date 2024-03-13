6df33b88ac311d4899b2380be0c80c117a8b5716
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-03-13 16:03:29 +0530

    inheritance complete

--- Python_Concepts/OOPS/03_Inheritance.py
+++ Python_Concepts/OOPS/03_Inheritance.py
@@ -4,11 +4,20 @@ class A:
     def displayA(self):
         print("Welcome to class A")
 
-
 class B(A):
     def displayB(self):
         print("Welcome to class B")
 
-obj1 = B() # Single level Inheritance 
-obj1.displayB()
-obj1.displayA()
+# class C(A,B): # if not using multilevel inheritance
+class C(B):
+    def displayC(self):
+        print("Welcome to class C")
+
+# obj1 = B() # Single level Inheritance 
+# obj1.displayA()
+# obj1.displayB()
+        
+obj2 = C() # Multilevel Inheritance
+obj2.displayA()
+obj2.displayB()
+obj2.displayC()
