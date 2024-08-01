37c6f2de6543fd0ac9cea5abe38874047a1d99e4
Shubham4051 <shubhamadarsh6017@gmail.com>
2024-08-01 11:38:21 +0530

    Got to Know about pylint and google python style_guide

--- Python_Concepts/Basics/L_1/1_Hello.py
+++ Python_Concepts/Basics/L_1/1_Hello.py
@@ -1,8 +1,12 @@
+'''
+docstring for the file
+'''
+
 print("Hello Buds !!\n143")
 print("Again i'm here \100 ")
 print("hi \77 \a" )
-my_name = "Shubham Adarsh"
-print("My name is : ", my_name)
-my_age = 24
-print(type(my_age), my_age)
+MY_NAME = "Shubham Adarsh"
+print("My name is : ", MY_NAME)
+MY_AGE = 24
+print(type(MY_AGE), MY_AGE)
 print("I'm a male,", bool(1), " !!")
