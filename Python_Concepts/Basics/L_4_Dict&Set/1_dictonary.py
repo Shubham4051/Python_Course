3c0af1c39cd9cf0f7ba4313b06409c86d39670b2
Shubham4051 <shubhamadarsh6017@gmail.com>
2024-08-12 21:07:31 +0530

    Dictonary Done

---
+++ Python_Concepts/Basics/L_4_Dict&Set/1_dictonary.py
@@ -0,0 +1,19 @@
+# key:value pairs
+info = {
+    "name" : "Shubham Adarsh",
+    "age" : 22,
+    "course" : "B-Tech",
+    "dept" : "CSE-AIML",
+    "language" : {
+        "python" : 1,
+        "Java" : 2,
+        "SQL" : 3
+    },
+    "cgpa" : 7.9,
+    "is_working" : False,
+    123 : 933
+}
+info["name"] = "Shubham"
+info["sur_name"] = "Adarsh"
+print(info)
+print(info["language"])
